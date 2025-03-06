bc7cdf0444a6cfa492710f887edf6c4105208ff1
LARISSA SCHMITZ <larissa_schmitz1@estudante.sesisenai.org.br>
2025-03-06 08:48:26 -0300

    sla

---
+++ ativ 1.js
@@ -0,0 +1,24 @@
+function calcularDigitos(cpf) {
+    var soma = 0, peso = 10
+
+    // Calcula o primeiro dígito
+    for (var i = 0; i < 9; i++) {
+        soma += cpf[i] * peso--
+    }
+    var primeiro = soma % 11 < 2 ? 0 : 11 - (soma % 11)
+
+    
+    cpf += primeiro
+
+    soma = 0, peso = 11
+    // Calcula o segundo dígito
+    for (var i = 0; i < 10; i++) {
+        soma += cpf[i] * peso--
+    }
+    var segundo = soma % 11 < 2 ? 0 : 11 - (soma % 11)
+
+    return primeiro + "" + segundo
+}
+
+
+console.log(calcularDigitos("123456789"))
